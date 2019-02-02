5bb13db3b22cd24cd998b3692f3e2156d381361c
Niv Gelbermann <niv.gelbermann@gmail.com>
2019-02-02 18:03:33 +0200

    basic version operational

--- bookmysel.py
+++ bookmysel.py
@@ -3,12 +3,12 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait as wait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver import ActionChains
-import time
+from selenium.common.exceptions import TimeoutException
+# from selenium.webdriver import ActionChains
+# import time
 
 
 def login(username, password):
-
     def fill_element(name, value): 
         elem = driver.find_element_by_name(name)
         elem.clear()
@@ -19,108 +19,77 @@ def login(username, password):
     try:
         login_btn = wait(driver, 10) \
                     .until(EC.element_to_be_clickable((By.XPATH, "//input[@id='idSIButton9']")))
-    except Exception as e:
-        handle_errors(e)
+    except TimeoutException as e:
+        report_failure(e)
     else:
         login_btn.send_keys(Keys.ENTER)
     driver.find_element_by_class_name("btn-primary").click() # "yes" button on login check
 
 
-def search_this_week():
-    time_btns = driver.find_elements_by_xpath("//label[@class='btn btn-default']")
-    time_btns[1].click() # select "thisweek" button
-    input_min = driver.find_element_by_xpath("//input[@class='form-control hours-minutes' and @id='minutes']")
-    input_min.clear()
-    input_min.send_keys("180") # TODO change to arg
-    submit_btn = driver.find_element_by_name("SUBMIT")
-    submit_btn.click()  
-
-
-def search_dates():
+def search_date(date, length):
     time_btns = driver.find_elements_by_xpath("//label[@class='btn btn-default']")
     time_btns[2].click() # select "thisweek" button
 
     begin_date = driver.find_element_by_xpath("//input[@type='text' and @id='beginDate']")
     begin_date.clear()
-    begin_date.send_keys("17/02/2019")
+    begin_date.send_keys(date)
     end_date = driver.find_element_by_xpath("//input[@type='text' and @id='endDate']")
     end_date.clear()
-    end_date.send_keys("23/02/2019")
+    end_date.send_keys(date)
     
     footer = driver.find_element_by_xpath("//footer")
     footer.click()
 
     input_min = driver.find_element_by_xpath("//input[@class='form-control hours-minutes' and @id='minutes']")
     input_min.clear()
-    input_min.send_keys("180") # TODO change to arg
+    input_min.send_keys(length) # TODO change to arg
     submit_btn = driver.find_element_by_name("SUBMIT")
     submit_btn.send_keys(Keys.ENTER)
 
 
-def handle_errors(e):
+def verify_success():
+    try:
+        wait(driver, 20).until(EC.presence_of_element_located(By.XPATH, "//input[@id='btnSaveSuccessful']"))
+    except TimeoutException as e:
+        return False    
+    else:
+        return True
+
+
+def report_failure(error=None):
     # TODO send mail about error 
     # TODO terminate browser and script
-    pass
+    print("FAILED: " + str(error))
 
 
 if __name__ == "__main__":
     driver = webdriver.Chrome()
     driver.get("https://bookme.technion.ac.il/booked/Web/search-availability.php")
-    if not "BookMe" in driver.title: # login required
+    if not "BookMe" in driver.title: 
         login("gelbermann@campus.technion.ac.il", "Fk1jhhk1") # TODO change to args
-        assert "BookMe" in driver.title
 
-    # # search_this_week()
-    # search_dates()
+    beginning = '13:00' # TODO change to arg
 
-    # beginning = '13:00' # TODO change to arg
-    # try:
-    #     # room ids:
-    #     # resourceid='8' - Alan Turing
-    #     # resourceid='9' - Ada Lovalace
-    #     # resourceid='10' - John Von Neumann
-    #     # resourceid='11' - Claude Shannon
-    #     openings = wait(driver, 30).until(EC.presence_of_all_elements_located((By.XPATH, \
-    #         "//div[@class='opening' and @data-resourceid='9' and contains(@data-startdate, '13:00')]")))
-    # except Exception as e:
-    #     handle_errors(e)
-    # else:
-    #     actions = ActionChains(driver)
-    #     for opening in openings:
-    #         # opening.click()
-    #         actions.key_down(Keys.CONTROL).click(opening).key_up(Keys.CONTROL).perform()
-    #         driver.switch_to.window(driver.window_handles[1])
-
-    #         before_checkbox = driver.find_element_by_xpath("//label[@for='startReminderEnabled']")
-    #         before_checkbox.click()
-    #         driver.find_elements_by_xpath("//button[contains(@class, 'save')]")[1].click()
-
-    #         driver.back()
-    #         time.sleep(5)
-    
-    actions = ActionChains(driver)
-    for i in range(0,6):
-        # search_this_week()
-        search_dates()
-
-        beginning = '13:00' # TODO change to arg
+    # room ids:
+    # resourceid='8' - Alan Turing (big room)
+    # resourceid='9' - Ada Lovalace
+    # resourceid='10' - John Von Neumann
+    # resourceid='11' - Claude Shannon
+    for room_id in ['9','10','11','8']:
+        search_date("17/02/2019", "180") # TODO change to args
         try:
-            # room ids:
-            # resourceid='8' - Alan Turing
-            # resourceid='9' - Ada Lovalace
-            # resourceid='10' - John Von Neumann
-            # resourceid='11' - Claude Shannon
-            openings = wait(driver, 30).until(EC.presence_of_all_elements_located((By.XPATH, \
-                "//div[@class='opening' and @data-resourceid='9' and contains(@data-startdate, '13:00')]")))
+            opening = wait(driver, 30).until(EC.presence_of_element_located((By.XPATH, \
+                f"//div[@class='opening' and @data-resourceid='{room_id}' and contains(@data-startdate, '{beginning}')]")))
         except Exception as e:
-            handle_errors(e)
+            report_failure(e)
         else:
-            openings[i].click()
+            opening.click()
+            driver.find_element_by_xpath("//label[@for='startReminderEnabled']").click() 
+            driver.find_elements_by_xpath("//button[contains(@class, 'save')]")[1].click() 
+            if verify_success():
+                driver.quit()
+                exit()
+            else:
+                driver.back()
 
-            before_checkbox = driver.find_element_by_xpath("//label[@for='startReminderEnabled']")
-            before_checkbox.click()
-            driver.find_elements_by_xpath("//button[contains(@class, 'save')]")[1].click()
-
-            driver.back()
-            time.sleep(5)
-        break # TODO remove
+    report_failure()
