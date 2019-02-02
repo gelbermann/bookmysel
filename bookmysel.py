077824bfb01835184e8a36ad0f3655120770a8a5
Niv Gelbermann <niv.gelbermann@gmail.com>
2019-02-02 18:44:45 +0200

    Add mail on failure

--- bookmysel.py
+++ bookmysel.py
@@ -6,6 +6,9 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 # from selenium.webdriver import ActionChains
 # import time
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 
 def login(username, password):
@@ -20,7 +23,7 @@ def login(username, password):
         login_btn = wait(driver, 10) \
                     .until(EC.element_to_be_clickable((By.XPATH, "//input[@id='idSIButton9']")))
     except TimeoutException as e:
-        report_failure(e)
+        report_failure("Login Failure", error=e)
     else:
         login_btn.send_keys(Keys.ENTER)
     driver.find_element_by_class_name("btn-primary").click() # "yes" button on login check
@@ -56,10 +59,30 @@ def verify_success():
         return True
 
 
-def report_failure(error=None):
-    # TODO send mail about error 
-    # TODO terminate browser and script
-    print("FAILED: " + str(error))
+def report_failure(reason, date="--/--/----", hour="--:--", error=None):
+    if 'server' not in vars() and 'server' not in globals():
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.ehlo()
+        server.starttls()
+        server.ehlo()
+        server.login("bookmysel", "seleniumrocks") # TODO change to args
+
+    fromaddr = "bookmysel@gmail.com" # TODO change to arg
+    toaddr = "gelbermann@campus.technion.ac.il" # TODO change to arg
+    msg = MIMEMultipart()
+    msg['From'] = fromaddr
+    msg['To'] = toaddr
+    msg['Subject'] = f"BookMe Reservation Failure for {date}"
+    body = f"Uh oh!\n\nScheduling a reservation for {date} at {hour} failed because of {reason}.\n\n" 
+    if not error is None:
+        body = body + f"Further details:\n{str(error)}"
+    body = str(body)
+    msg.attach(MIMEText(body, 'plain'))
+
+    server.sendmail(fromaddr, toaddr, msg.as_string())
+    print(f"Failed to make reservation because: \n{str(error)}") # TODO fix empty error message
+    driver.quit()
+    exit()
 
 
 if __name__ == "__main__":
@@ -68,28 +91,28 @@ if __name__ == "__main__":
     if not "BookMe" in driver.title: 
         login("gelbermann@campus.technion.ac.il", "Fk1jhhk1") # TODO change to args
 
-    beginning = '13:00' # TODO change to arg
+    hour = '13:00' # TODO change to arg
 
-    # room ids:
-    # resourceid='8' - Alan Turing (big room)
-    # resourceid='9' - Ada Lovalace
-    # resourceid='10' - John Von Neumann
-    # resourceid='11' - Claude Shannon
-    for room_id in ['9','10','11','8']:
+    rooms = {'9': 'Ada Lovelace',
+            '10': 'John Von Neumann',
+            '11': 'Claude Shannon',
+             '8': 'Alan Turing'}
+    for room_id in rooms.keys():
         search_date("17/02/2019", "180") # TODO change to args
         try:
             opening = wait(driver, 30).until(EC.presence_of_element_located((By.XPATH, \
                 f"//div[@class='opening' and @data-resourceid='{room_id}' and contains(@data-startdate, '{beginning}')]")))
-        except Exception as e:
-            report_failure(e)
+        except TimeoutException as e:
+            pass
         else:
             opening.click()
             driver.find_element_by_xpath("//label[@for='startReminderEnabled']").click() 
             driver.find_elements_by_xpath("//button[contains(@class, 'save')]")[1].click() 
             if verify_success():
+                print(f"Reservation made for room {rooms[room_id]} {date} at {hour}.")
                 driver.quit()
                 exit()
             else:
                 driver.back()
 
-    report_failure()
+    report_failure("No Successful Reservation")
